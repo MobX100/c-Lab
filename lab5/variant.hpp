ec7e541dd44d6bb79847faa3455f1d4f21badd9e
MobX100 <104301504+MobX100@users.noreply.github.com>
2025-12-17 16:37:22 +0300

    Update variant.hpp

--- lab5/variant.hpp
+++ lab5/variant.hpp
@@ -8,121 +8,102 @@
 #include <string>
 #include <type_traits>
 
-#include <boost/variant.hpp>
-#include <boost/recursive_wrapper.hpp>
-
-//{ number - вариантное число (int или float)
+//{ number
 using number = std::variant<int, float>;
 //}
 
-//{ array - массив чисел
+//{ array
 using array = std::vector<number>;
 //}
 
-//{ recursive_array - рекурсивный массив (версия с shared_ptr)
-struct recursive_array : public std::variant<
+//{ recursive_array
+// Uses std::shared_ptr for recursion
+struct recursive_array;
+
+using recursive_array_value = std::variant<
     number,
     std::shared_ptr<recursive_array>
-> {
-    using base = std::variant<number, std::shared_ptr<recursive_array>>;
-    using base::base;
-    using base::operator=;
-    
-    // Оператор индексирования для тестов
-    recursive_array& operator[](size_t index) {
-        if (auto* arr = std::get_if<std::shared_ptr<recursive_array>>(this)) {
-            return (*arr)->operator[](index);
-        }
-        throw std::bad_variant_access();
-    }
-    
-    const recursive_array& operator[](size_t index) const {
-        if (auto* arr = std::get_if<std::shared_ptr<recursive_array>>(this)) {
-            return (*arr)->operator[](index);
-        }
-        throw std::bad_variant_access();
-    }
+>;
+
+struct recursive_array : std::vector<recursive_array_value>
+{
+    using std::vector<recursive_array_value>::vector;
 };
 //}
 
-//{ recursive_array2 - рекурсивный массив (версия с recursive_wrapper)
-struct recursive_array2 : public boost::variant<
+//{ recursive_array2
+// Uses boost::recursive_wrapper for recursion
+#include <boost/variant/recursive_wrapper.hpp>
+
+struct recursive_array2;
+
+using recursive_array2_value = std::variant<
     number,
     boost::recursive_wrapper<recursive_array2>
-> {
-    using base = boost::variant<number, boost::recursive_wrapper<recursive_array2>>;
-    using base::base;
-    using base::operator=;
-    
-    // Оператор индексирования
-    recursive_array2& operator[](size_t index) {
-        if (auto* arr = boost::get<boost::recursive_wrapper<recursive_array2>>(this)) {
-            return arr->get()[index];
-        }
-        throw std::bad_variant_access();
+>;
+
+struct recursive_array2 : std::vector<recursive_array2_value>
+{
+    using std::vector<recursive_array2_value>::vector;
+};
+//}
+
+//{ variant_decorator
+template<typename... Ts>
+struct variant_decorator
+{
+    using variant_type = std::variant<Ts...>;
+    variant_type var;
+
+    constexpr variant_decorator() = default;
+
+    template<typename U, typename = std::enable_if_t<
+        std::disjunction_v<std::is_constructible<Ts, U>...>
+    >>
+    constexpr variant_decorator(U&& value) : var(std::forward<U>(value)) {}
+
+    // Non-const and const overloads via common private helper (Meyers-style)
+private:
+    template<typename T>
+    constexpr T& _get_as() & {
+        return std::get<T>(var);
     }
-    
-    const recursive_array2& operator[](size_t index) const {
-        if (auto* arr = boost::get<boost::recursive_wrapper<recursive_array2>>(this)) {
-            return arr->get()[index];
-        }
-        throw std::bad_variant_access();
+
+    template<typename T>
+    constexpr const T& _get_as() const& {
+        return std::get<T>(var);
+    }
+
+public:
+    template<typename T>
+    constexpr T& as() & {
+        return _get_as<T>();
+    }
+
+    template<typename T>
+    constexpr const T& as() const& {
+        return _get_as<T>();
     }
 };
 //}
 
-//{ variant_decorator - декоратор для variant
-template<typename... Types>
-struct variant_decorator : public std::variant<Types...> {
-    using base = std::variant<Types...>;
-    using base::base;
-    using base::operator=;
-    
-    // Метод as() для безопасного получения значения
-    template<typename T>
-    const T& as() const {
-        return std::get<T>(*this);
-    }
-    
-    template<typename T>
-    T& as() {
-        return std::get<T>(*this);
-    }
-};
-//}
+//{ recursive_map
+// Must be a map from string to a variant that supports .as<T>()
+// We define a variant type that includes all leaf types + recursive containers
+struct recursive_map;
 
-//{ recursive_map - рекурсивная карта (ассоциативный массив)
-struct recursive_map : public std::map<
+using recursive_map_value = variant_decorator<
+    int,
+    float,
+    bool,
     std::string,
-    boost::variant<
-        int,
-        float,
-        std::string,
-        bool,
-        boost::recursive_wrapper<recursive_map>
-    >
-> {
-    using value_type = boost::variant<
-        int,
-        float,
-        std::string,
-        bool,
-        boost::recursive_wrapper<recursive_map>
-    >;
-    using base = std::map<std::string, value_type>;
-    using base::base;
-    using base::operator=;
-    
-    // Метод as() для удобного доступа
-    template<typename T>
-    const T& as(const std::string& key) const {
-        return boost::get<T>(this->at(key));
-    }
-    
-    template<typename T>
-    T& as(const std::string& key) {
-        return boost::get<T>(this->at(key));
-    }
+    boost::recursive_wrapper<recursive_array2>,
+    boost::recursive_wrapper<recursive_map>
+>;
+
+struct recursive_map : std::map<std::string, recursive_map_value>
+{
+    using std::map<std::string, recursive_map_value>::map;
 };
 //}
 
